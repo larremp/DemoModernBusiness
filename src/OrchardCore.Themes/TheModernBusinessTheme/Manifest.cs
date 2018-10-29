a70bde853cb7dd13595ea2da5cea29a788408505
larremp <larremp@gmail.com>
2018-10-29 10:47:08 -0500

    add the files

---
+++ src/OrchardCore.Themes/TheModernBusinessTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Modern Business Theme",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "0.1.0",
+    Description = "A theme adapted for modern business requirements."
+)]
